a4b8fdc5fdb11f23104284f1e1985ecb6d0a79aa
Tom Kent <tom@teeks99.com>
2018-12-31 21:32:35 -0600

    Better deps output

--- get_deps.py
+++ get_deps.py
@@ -5,6 +5,7 @@ try:
 except ImportError: # Python 2
     from urllib import urlretrieve
 
+print("Getting Source")
 src_url = "https://boost.teeks99.com/lib/1.69.0/boost_1_69_0.tar.bz2"
 src_file = "boost_1_69_0.tar.bz2"
 urlretrieve(src_url, src_file)
@@ -12,6 +13,7 @@ t = tarfile.open(src_file)
 t.extractall()
 t.close()
 
+print("Getting Binaries")
 bin_url = "https://boost.teeks99.com/misc/inno_test/boost-bin-msvc14.1-all.tar.xz"
 bin_file = "boost-bin-msvc14.1-all.tar.xz"
 urlretrieve(bin_url, bin_file)
@@ -19,6 +21,7 @@ t = tarfile.open(bin_file)
 t.extractall()
 t.close()
 
+print("Making combined")
 shutil.copytree("boost_1_69_0", "boost_combined")
 shutil.copytree("boost-bin-msvc14.1-all/lib32-msvc-14.1", "boost_combined/lib32-msvc-14.1")
 shutil.copytree("boost-bin-msvc14.1-all/lib64-msvc-14.1", "boost_combined/lib64-msvc-14.1")
